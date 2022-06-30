c94cb44c900d3e450bd9ebb4c0fd39d40f882d49
Keks <keks@htmlacademy.ru>
2022-06-30 15:50:44 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,71 @@
+const returnNumber = (firstNumber,secondNumber) => {
+  if ((secondNumber - firstNumber) >= 0) {
+    const randomNumb = Math.round(Math.random() * (secondNumber - firstNumber) + firstNumber);
+    return randomNumb;
+  }
+  return(-1);
+} ;
+returnNumber(1,10);
+
+const strLength = (ourStr, maxStr) =>
+{  ourStr = String(ourStr);
+  return ourStr.length <= maxStr.length;
+} ;
+strLength('ff','ssd');
+
+const getRandomPositiveInteger = (a,b) =>{
+  const lower = Math.ceil(Math.min(Math.abs(a),Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const ID = [];
+const URL = [];
+const AVATAR = [];
+for(let i = 0; i<=25; i++){
+  ID[i]=i+1;
+  URL[i] = `photos/${i+1}.jpg`;
+}
+for(let i = 0; i<=6;i++){
+  AVATAR[i] = `avatar-${i+1}.jpg`;
+}
+const DESCRIPTION = [
+  'Осень' ,
+  'Поездка в даль' ,
+  'Лучшие моменты в моей жизни' ,
+  'Унылая пора',
+  'Тёплые денёчки' ,
+  'Опять поезда'
+];
+const NAME = [
+  'Алексей' ,
+  'Владимир',
+  'Сергей' ,
+  'Георгий' ,
+  'Ирина' ,
+  'Вероника'
+];
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.'
+];
+
+const getRandomArrayElement = (elements) => elements [getRandomPositiveInteger(0, elements.length - 1)];
+const COMMENTS = [
+  {id:getRandomArrayElement(NAME.length),
+    message:getRandomArrayElement(MESSAGE),
+    avatar:getRandomArrayElement(AVATAR),
+    name:getRandomArrayElement(NAME)
+  }];
+const createReview = () => ({
+  id:getRandomArrayElement(ID),
+  url:getRandomArrayElement(URL),
+  description : getRandomArrayElement(DESCRIPTION),
+  likes: getRandomPositiveInteger (15,250),
+  comments:getRandomArrayElement(COMMENTS)
+});
+// eslint-disable-next-line no-unused-vars
+const COMMENTARIES=Array.from({length:10},createReview);
+
+
